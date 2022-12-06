84392fe43f4d9b01c3412fc79d80d666ae571257
Philipp Daun <post@philippdaun.net>
2022-12-06 01:06:58 +0100

    Formatting

--- cli/cli.js
+++ cli/cli.js
@@ -1,18 +1,18 @@
 import shell from 'shelljs';
 
 export function exec(command) {
-  return shell.exec(command);
+	return shell.exec(command);
 }
 
 export function echo(message) {
-  shell.echo(`[swup] ${message}`);
+	shell.echo(`[swup] ${message}`);
 }
 
 export function error(message) {
-  if (Array.isArray(message)) {
-    message.forEach(msg => error(msg));
-  } else {
-    echo(`Error: ${message}`);
-  }
-  shell.exit(1);
+	if (Array.isArray(message)) {
+		message.forEach(msg => error(msg));
+	} else {
+		echo(`Error: ${message}`);
+	}
+	shell.exit(1);
 }
