22a0e2f83c9b1f9852ae044e722655a63817eaec
Philipp Daun <post@philippdaun.net>
2023-01-08 21:49:39 +0100

    Remove microbundle JS integration

--- bin/commands/build.js
+++ bin/commands/build.js
@@ -1,24 +1,7 @@
-import microbundle from 'microbundle';
-
 import { exec, echo } from '../lib/shell.js';
-import { resolve } from '../lib/helpers.js';
 
 export default async function build() {
 	echo('Bundling plugin with microbundle');
-
-	// await microbundle({
-	// 		cwd: resolve(),
-	// 		// input: resolve('./src/index.js'),
-	// 		// output: resolve('./dist/'),
-	// 		formats: 'modern,esm,cjs',
-	// });
-	// await microbundle({
-	// 		cwd: resolve(),
-	// 		// input: resolve('./src/index.js'),
-	// 		// output: resolve('./dist/'),
-	// 		formats: 'umd',
-	// 		external: 'none',
-	// });
 	exec('microbundle -f modern,esm,cjs');
 	exec('microbundle -f umd --external none');
 	return true;
