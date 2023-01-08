2c8d181a093f51711edec95aad0e1142abb74b93
Philipp Daun <post@philippdaun.net>
2023-01-08 21:50:34 +0100

    Remove mjs files from prettier command

--- bin/commands/lint.js
+++ bin/commands/lint.js
@@ -2,6 +2,6 @@ import { exec, echo } from '../lib/shell.js';
 
 export default async function lint() {
 	echo('Linting plugin with prettier');
-	exec('prettier src/**/*.{js,mjs} --write');
+	exec('prettier src/**/*.js --write');
 	return true;
 };
