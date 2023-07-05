7f70fbc2e1548762c651602bde0732bb46913562
Rasso Hilber <mail@rassohilber.com>
2023-07-05 15:50:51 +0200

    Update `format` script

--- bin/commands/format.js
+++ bin/commands/format.js
@@ -2,6 +2,6 @@ import { exec, echo } from '../lib/shell.js';
 
 export default async function format() {
 	echo('Formatting with prettier');
-	exec('prettier src/**/*.js --write');
+	exec(`prettier 'src/**/*.{js,ts,mjs}' --write`);
 	return true;
 };
