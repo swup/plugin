2095ff337625c58d4cae123abb1775d5431246ad
Philipp Daun <post@philippdaun.net>
2023-07-15 14:53:59 +0200

    Install vitest

---
+++ vitest/vitest.config.ts
@@ -0,0 +1,15 @@
+/**
+ * Vitest config file
+ * @see https://vitest.dev/config/
+ */
+
+import path from "path";
+import { defineConfig } from 'vitest/config';
+
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+	test: {
+		environment: 'jsdom'
+	}
+});
